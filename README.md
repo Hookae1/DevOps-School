db203fa0f8429628501cd1778158a46ef27f0918
Hookae1 <rybitskyiy@gmail.com>
2021-11-25 18:43:07 +0000

    The first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DevOps Authum School 2021
+
+**All labs and other staff would be stored here**
+
+-------------------------------------------------
+## LAB 1
+
+:one: First task you could find via link below:
+
+[Practical test #1](https://docs.google.com/document/d/1VC8S0aYAFk_GECu6n4Thg5tLrHPyH4YjFjYlN-8ch44/edit?usp=sharing)
+
+:dart: The check the solution, please [:computer_mouse:HERE](https://github.com/Hookae1/DevOps-School/blob/master/lab1/test1.sh)
---
+++ lab1/test1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Tell to the script to stop processing in case of non-true value
+set -e
+
+#Enter <user name>
+read -p "Enter user name: " x
+
+#User ckecking function
+
+user_check(){
+ 	if id "$x" &>/dev/null; then
+		echo "User '$x' exists" >/dev/null
+	else
+		echo -en "\033[31;47;1m User '$x' doesn't exists"
+		exit 1
+	fi
+}
+
+user_check '$x'
+
+#Enter <directory name>
+read -p "Enter directory name: " y
+
+#Directory checking function
+
+dir_check(){
+	if [ -d "$y" ]; then
+		echo "Directory "$y"' exist" >/dev/null
+	else
+		echo -en "\033[31;47;1m Directory "$y" doesn't exist"
+		exit 1
+	fi
+}
+
+dir_check '$y'
+
+#If both of the paramters is TRUE - Recursive change owner of the folder
+
+if user_check "$"x && dir_check "$y"; then
+	sudo chown -R "$x" "$y" &>/dev/null 
+	echo -en "\033[42;34;2m !!User "$" was Successfully set for Directory "$y"!!"
+fi
